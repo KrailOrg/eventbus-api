e9108ff83fe71d982ddf82771e8c7b48f4780763
David Sowerby <david.sowerby@virgin.net>
2018-01-10 09:18:25 +0000

    Fix #5 SubscribeTo javadoc expanded

--- src/main/kotlin/uk/q3c/krail/eventbus/SubscribeTo.kt
+++ src/main/kotlin/uk/q3c/krail/eventbus/SubscribeTo.kt
@@ -11,15 +11,24 @@
 
 package uk.q3c.krail.eventbus
 
+import com.google.inject.spi.InjectionListener
 import java.lang.annotation.Inherited
 import kotlin.reflect.KClass
 
 /**
- * Overrides default behaviour for subscribing a [Listener] to an Event Bus of a specific scope (see EventBusModule in the implementation package} for description of default
- * behaviour).  The only valid values in the base implementation is [GlobalMessageBus].  Any other values are silently ignored.
+ * Overrides default behaviour for subscribing a [Listener] to an Event Bus of a specific scope or message type, if provided.
+ * Subscription is provided by an implementation of [EventBusAutoSubscriber], a Guice [InjectionListener]
  *
+ * Default subscription annotations provided by this interface are [GlobalMessageBus] and [GlobalEventBus].
  *
- * Because this annotation overrides default behaviour, if this annotation has no values, then the [Listener] will not be subscribed to any bus.
+ * The behaviour expected of an implementation is:
+ *
+ * - A **@Listener** without any **@SubscribeTo** will cause the Listener to be subscribed to the default, [GlobalMessageBus]
+ * - A **@Listener** with an empty **@SubscribeTo()** will not be subscribed to any bus, and therefore cause any inherited annotation to be ignored.
+ * - A **@Listener** with a specified **@SubscribeTo(XBus.class)** will be subscribed only to XBus, ignoring any inherited annotation.
+ *
+ * Invalid parameters to **@SubscribeTo** (that is, they are not recognised as bus annotations) are silently ignored.
+ *
  *
  *
  * Created by David Sowerby on 09/03/15.
